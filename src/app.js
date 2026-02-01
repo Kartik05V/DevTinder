2acf7a38d8e6cfbb370cadead33ca2bd63343326
Kartik Varshney <codehacker05@gmail.com>
2026-02-01 19:53:47 +0530

    Validation in all API's

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
-const { default: mongoose } = require("mongoose");
+//const { default: mongoose } = require("mongoose");
+const { validateSignUpData } = require("./utils/validate.js");
+const bcrypt = require("bcrypt");
 
 
 
@@ -28,15 +30,47 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req,res)=>{
-    // Creating a new instance of the User model
-    const user = new User(req.body);
-
     try{
+        validateSignUpData(req);
+        const { firstName, lastName, emailId, password } = req.body;
+        
+        // Encrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
+        console.log(passwordHash);
+
+         //   Creating a new instance of the User model
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+        });
+
         await user.save();
         res.send("User registered successfully")
     } catch (err){
         res.status(400).send("Error Saving the user: "+ err.message);
     }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials email");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      res.send("Login Successful!!!");
+    } else {
+      throw new Error("Invalid credentials password");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
 });
 
 app.get("/user", async (req,res)=>{
@@ -77,30 +111,60 @@ app.delete("/user",async (req,res)=>{
     };
 });
 
-app.patch("/user", async (req,res)=>{
+app.patch("/user/:userId", async (req,res)=>{
     const data = req.body;
-    const userId = req.body.userId;
+    const userId = req.params.userId;
     // any other data field that is sent in the body of the request 
     // that is not in the schema will be ignored by the api. 
 
     try{
-        const user = await User.findByIdAndUpdate({_id : userId}, data, {returnDocument:"after"});
+        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((k) =>ALLOWED_UPDATES.includes(k));
+        if (!isUpdateAllowed) {
+            throw new Error("Update not allowed");
+        }
+        if (data?.skills?.length > 10) {
+            throw new Error("Skills cannot be more than 10");
+        }
+        if (data?.skills && data?.skills?.length > 0) {
+            const uniqueSkills = [...new Set(data?.skills)];
+            if (uniqueSkills.length !== data?.skills.length) {
+                throw new Error("Duplicate skills are not allowed");
+            };
+        }
+        const user = await User.findByIdAndUpdate({_id : userId}, data,
+             {returnDocument:"after", runValidators : true});
         res.send("User updated successfully");
     }
     catch (err){
-        res.status(400).send("Something went wrong");
+        res.status(400).send("Update Failed "+ err.message);
     };
 });
 
-app.patch("/userbyemail", async (req,res)=>{
+app.patch("/userbyemail/:useremail", async (req,res)=>{
     const data = req.body;
-    const userId = req.body.userId;
+    const useremail = req.params.useremail;
     try{
-        const user = await User.findOneAndUpdate({_id : userId}, data, {returnDocument:"after"});
+        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((k) =>ALLOWED_UPDATES.includes(k));
+        if (!isUpdateAllowed) {
+            throw new Error("Update not allowed");
+        }
+        if (data?.skills?.length > 10) {
+            throw new Error("Skills cannot be more than 10");
+        }
+        if (data?.skills && data?.skills?.length > 0) {
+            const uniqueSkills = [...new Set(data?.skills)];
+            if (uniqueSkills.length !== data?.skills.length) {
+                throw new Error("Duplicate skills are not allowed");
+            };
+        }
+        const user = await User.findOneAndUpdate({emailId : useremail}, data, {returnDocument:"after" ,
+             runValidators : true });
         res.send("User updated successfully by emailId");
     }
     catch (err){
-        res.status(400).send("Something went wrong");
+        res.status(400).send("Update Failed "+ err.message);
     };
 });
 
