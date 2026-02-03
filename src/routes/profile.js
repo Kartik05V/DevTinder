eeca932a61db8ea8ae7fe753301171aa930de9c1
Kartik Varshney <codehacker05@gmail.com>
2026-02-03 17:06:45 +0530

    Logout and Profile Api's

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,8 +1,10 @@
 const {userAuth} = require("../middlewares/auth.js");
-const express = require('express')
+const express = require('express');
+const { validateEditProfileData } = require("../utils/validate.js");
+const user = require("../models/user.js");
 const profileRouter = express.Router()
 
-profileRouter.get("/profile",userAuth ,async (req,res)=>{
+profileRouter.get("/profile/view",userAuth ,async (req,res)=>{
     try {
         const user = req.user;
         res.send(user);
@@ -10,5 +12,28 @@ profileRouter.get("/profile",userAuth ,async (req,res)=>{
         res.status(400).send("ERROR : " + err.message);
     };
 });
+
+profileRouter.patch("/profile/edit",userAuth,async (req,res)=>{
+    try{
+        validateEditProfileData(req);
+        if (!validateEditProfileData(req)) {
+            throw new Error("Invalid Edit Request");
+        }
+        loggedInUser= req.user;
+        Object.keys(req.body).forEach((key)=>{
+            loggedInUser[key]=req.body[key];
+        });
+        
+        await loggedInUser.save();
+
+        res.json({
+            message: `${loggedInUser.firstName}, your profile updated successfuly`,
+            data: loggedInUser,
+        });
+    }catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+    
 
 module.exports = profileRouter;
