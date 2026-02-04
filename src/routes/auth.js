b4d7cf15c40bf65b295b604059837d557135c7a2
Kartik Varshney <codehacker05@gmail.com>
2026-02-04 19:42:56 +0530

    Created Feed Api and Change Password Api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,31 +1,31 @@
 const { validateSignUpData } = require("../utils/validate.js");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
-const express = require('express')
-const authRouter = express.Router()
+const express = require("express");
+const authRouter = express.Router();
 
-authRouter.post("/signup", async (req,res)=>{
-    try{
-        validateSignUpData(req);
-        const { firstName, lastName, emailId, password } = req.body;
-        
-        // Encrypt the password
-        const passwordHash = await bcrypt.hash(password, 10);
-        console.log(passwordHash);
+authRouter.post("/signup", async (req, res) => {
+  try {
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
 
-        //   Creating a new instance of the User model
-        const user = new User({
-            firstName,
-            lastName,
-            emailId,
-            password: passwordHash,
-        });
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    //console.log(passwordHash);
 
-        await user.save();
-        res.send("User registered successfully")
-    } catch (err){
-        res.status(400).send("Error Saving the user: "+ err.message);
-    }
+    //   Creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
+    await user.save();
+    res.send("User registered successfully");
+  } catch (err) {
+    res.status(400).send("Error Saving the user: " + err.message);
+  }
 });
 
 authRouter.post("/login", async (req, res) => {
@@ -40,28 +40,28 @@ authRouter.post("/login", async (req, res) => {
     const isPasswordValid = await user.validatePassword(password);
 
     if (isPasswordValid) {
-        // Create a JWT Token
-        //const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$05");
-        const token = await user.getJWT();
+      // Create a JWT Token
+      //const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$05");
+      const token = await user.getJWT();
 
-        // Add the token to cookie and send the response back to the user
-        res.cookie("token", token, {
-            expires: new Date(Date.now() + 8 * 3600000),//expire after 8 hrs
-        });
-        res.send("Login Successful!!!");
+      // Add the token to cookie and send the response back to the user
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000), //expire after 8 hrs
+      });
+      res.send("Login Successful!!!");
     } else {
       throw new Error("Invalid credentials ");
     }
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
-  };
+  }
 });
 
-authRouter.post("/logout",async (req,res)=>{
-    res.cookie("token",null,{
-        expires: new Date(Date.now()),
-    })
-    res.send("Logout successful ")
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.send("Logout successful ");
 });
 
 module.exports = authRouter;
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,39 +1,89 @@
-const {userAuth} = require("../middlewares/auth.js");
-const express = require('express');
+const { userAuth } = require("../middlewares/auth.js");
+const express = require("express");
 const { validateEditProfileData } = require("../utils/validate.js");
 const user = require("../models/user.js");
-const profileRouter = express.Router()
+const profileRouter = express.Router();
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
-profileRouter.get("/profile/view",userAuth ,async (req,res)=>{
-    try {
-        const user = req.user;
-        res.send(user);
-    }catch (err) {
-        res.status(400).send("ERROR : " + err.message);
-    };
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
 });
 
-profileRouter.patch("/profile/edit",userAuth,async (req,res)=>{
-    try{
-        validateEditProfileData(req);
-        if (!validateEditProfileData(req)) {
-            throw new Error("Invalid Edit Request");
-        }
-        loggedInUser= req.user;
-        Object.keys(req.body).forEach((key)=>{
-            loggedInUser[key]=req.body[key];
-        });
-        
-        await loggedInUser.save();
-
-        res.json({
-            message: `${loggedInUser.firstName}, your profile updated successfuly`,
-            data: loggedInUser,
-        });
-    }catch (err) {
-        res.status(400).send("ERROR : " + err.message);
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    validateEditProfileData(req);
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
     }
+    loggedInUser = req.user;
+    Object.keys(req.body).forEach((key) => {
+      loggedInUser[key] = req.body[key];
+    });
+
+    await loggedInUser.save();
+
+    res.json({
+      message: `${loggedInUser.firstName}, your profile updated successfuly`,
+      data: loggedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
 });
-    
+
+profileRouter.patch("/profile/updatepassword", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const oldPassword = req.body.oldPassword;
+    const newPassword = req.body.newPassword;
+
+    if (!oldPassword || !newPassword) {
+      throw new Error("Please provide both Old and New Passwords");
+    }
+
+    const isMatch = await loggedInUser.validatePassword(oldPassword);
+    if (!isMatch) {
+      throw new Error("Password Entered is Invalid");
+    }
+    //console.log(isMatch);
+
+    //const isSame = await loggedInUser.validatePassword(newPassword);
+    // if (isSame) {
+    //   throw new Error("New Password cannot be same as Old Password");
+    // }
+    if (oldPassword === newPassword) {
+      throw new Error("New Password cannot be same as Old Password");
+    }
+
+    if (!validator.isStrongPassword(newPassword)) {
+      throw new Error("Please enter a strong Password!");
+    }
+
+    const newHashPassword = await bcrypt.hash(newPassword, 10);
+    loggedInUser.password = newHashPassword;
+    await loggedInUser.save();
+
+    // SECURITY: INVALIDATE OLD SESSION
+    // A: Force Logout (Safer)
+    res.cookie("token", null, { expires: new Date(0) }); // Delete cookie
+
+    // B: Keep Logged In (Better UX) -> You must send a NEW token here
+    // const newToken = await loggedInUser.getJWT();
+    // res.cookie("token", newToken, {expires: new Date(Date.now() + 8 * 3600000),});
+
+    res.json({
+      message: "Password updated successfully. Please login again.",
+      success: true,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
 
 module.exports = profileRouter;
